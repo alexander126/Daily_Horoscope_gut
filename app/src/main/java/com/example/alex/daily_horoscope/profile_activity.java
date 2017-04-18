c9387eb5ee5bd8fae232322847d552905b8fc4f5
alexander126 <akalapsazov@yahoo.com>
2017-04-18 22:51:06 +0300

    Zodiac Sign Message

--- app/src/main/java/com/example/alex/daily_horoscope/profile_activity.java
+++ app/src/main/java/com/example/alex/daily_horoscope/profile_activity.java
@@ -96,11 +96,54 @@ public class profile_activity extends AppCompatActivity {
         long date = System.currentTimeMillis();
         SimpleDateFormat sdf = new SimpleDateFormat("MM dd, yyyy");
         String dateString = sdf.format(date);
+        //--Setting zodiac sign value--//
+        String sign = "";
+        Calendar myCalendar = Calendar.getInstance();
+        int month = myCalendar.get(Calendar.MONTH);
+        int day = myCalendar.get(Calendar.DAY_OF_MONTH);
+        month = month + 1;
+        if ((month == 12 && day >= 22 && day <= 31) || (month == 1 && day >= 1 && day <= 19)){
+            sign = "Capricorn";
+        }
+        else if ((month == 1 && day >= 20 && day <= 31) || (month == 2 && day >= 1 && day <= 17)){
+            sign = "Aquarius";
+        }
+        else if ((month == 2 && day >= 18 && day <= 29) || (month == 3 && day >= 1 && day <= 19)) {
+            sign = "Pisces";
+        }
+        else if ((month == 3 && day >= 20 && day <= 31) || (month == 4 && day >= 1 && day <= 19)) {
+            sign = "Aries";
+        }
+        else if ((month == 4 && day >= 20 && day <= 30) || (month == 5 && day >= 1 && day <= 20)) {
+            sign = "Taurus";
+        }
+        else if ((month == 5 && day >= 21 && day <= 31) || (month == 6 && day >= 1 && day <= 20)) {
+            sign = "Gemini";
+        }
+        else if ((month == 6 && day >= 21 && day <= 30) || (month == 7 && day >= 1 && day <= 22)) {
+            sign = "Cancer";
+        }
+        else if ((month == 7 && day >= 23 && day <= 31) || (month == 8 && day >= 1 && day <= 22)) {
+            sign = "Leo";
+        }
+        else if ((month == 8 && day >= 23 && day <= 31) || (month == 9 && day >= 1 && day <= 22)) {
+            sign = "Virgo";
+        }
+        else if ((month == 9 && day >= 23 && day <= 30) || (month == 10 && day >= 1 && day <= 22)) {
+            sign = "Libra";
+        }
+        else if ((month == 10 && day >= 23 && day <= 31) || (month == 11 && day >= 1 && day <= 21)) {
+            sign = "Scorpio";
+        }
+        else if ((month == 11 && day >= 22 && day <= 30) || (month == 12 && day >= 1 && day <= 21)) {
+            sign = "Sagittarius";
+        }
 
         String name = prefs.getString("myname","");
         TextView txtv1 = (TextView) findViewById(R.id.textView);
-
-        txtv1.setText("Hello " + name + " welcome to Daily Horoscope, the current date is: " + dateString);
+       // TextView txtv2 = (TextView) findViewById(R.id.textView1);
+        txtv1.setText("Hello " + name + " welcome to Daily Horoscope, the current date is: " + dateString + ". " +
+                "The current zociac sign is " + sign);
         //---Initiallizing the properties used in profile activity---//
 
         ImageButton btn1 = (ImageButton) findViewById(R.id.btn1);
