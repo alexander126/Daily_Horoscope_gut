1cbd1e56dd139634c4d2b32208cf9d226f4c5251
alexander126 <akalapsazov@yahoo.com>
2017-07-05 23:08:36 +0300

    Rate Button/Screen Support Progress

--- app/src/main/java/com/example/alex/daily_horoscope/SettingsFragment.java
+++ app/src/main/java/com/example/alex/daily_horoscope/SettingsFragment.java
@@ -3,10 +3,12 @@ package com.example.alex.daily_horoscope;
 import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.PendingIntent;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
@@ -33,6 +35,29 @@ public class SettingsFragment extends PreferenceFragment {
         //BUTTON ABOUT
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+        // Rate Button
+        findPreference("key4");
+        Preference key4 = (Preference) findPreference("key4");
+        key4.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener(){
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Uri uri = Uri.parse("market://details?id=" + getContext().getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                // To count with Play market backstack, After pressing back button,
+                // to taken back to our application, we need to add following flags to intent.
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + getContext().getPackageName())));
+                }
+                return true;
+            }
+        });
+        // About button
         findPreference("key3");
         Preference key3 = (Preference) findPreference("key3");
         key3.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
