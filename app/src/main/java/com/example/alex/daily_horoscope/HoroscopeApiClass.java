a45c145e278f89569d283bdab18dd85d7ed58dfe
Aleksandar Kalapsazov <aleks@gearedapp.co.uk>
2018-11-10 21:24:15 +0000

    🚧 Replace HTML with API requests for Horoscope

---
+++ app/src/main/java/com/example/alex/daily_horoscope/HoroscopeApiClass.java
@@ -0,0 +1,60 @@
+package com.example.alex.daily_horoscope;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class HoroscopeApiClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        final TextView mTextView = findViewById(R.id.testreq);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://aztro.sameerkumar.website/?sign=aries&day=today";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                        try {
+
+                            JSONObject jObj = new JSONObject(response);
+                            Log.d("DESC", jObj.getString("date_range"));
+                            mTextView.setText(jObj.getString("description"));
+
+                        } catch (JSONException e) {
+                            Log.e("MYAPP", "unexpected JSON exception", e);
+                            // Do something to recover ... or kill the app.
+                        }
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        );
+        queue.add(postRequest);
+
+    }
+}
