44268d111e8621c5fe5aaf1d078eda222ed80815
Aleksandar Kalapsazov <aleks@gearedapp.co.uk>
2018-11-13 18:03:47 +0000

    🚧API Replacements

--- app/src/main/java/com/example/alex/daily_horoscope/gemini_activity.java
+++ app/src/main/java/com/example/alex/daily_horoscope/gemini_activity.java
@@ -1,48 +1,63 @@
-/**
- * The gemini_activity class is used for displaying
- * information provided from BasicZodiacActivity
- * about the specific zodiac sign.
- *
- *
- * @author  Aleksandar Kalapsazov
- * @version 1.0
- * @since   2017-03-18
- */
 package com.example.alex.daily_horoscope;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
-public class gemini_activity extends BasicZodiacActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class gemini_activity extends AppCompatActivity {
 
     @Override
-    //---On create function which displays the formated string---//
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gemini_activity);
-        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        final TextView textView = (TextView) findViewById(R.id.gemini_textview);
+        String url = "https://aztro.sameerkumar.website/?sign=gemini&day=today";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                        try {
 
-        mAdView.loadAd(adRequest);
-        MobileAds.initialize(this, "ca-app-pub-8629737007792498/4456238739");
-        String link = "https://cafeastrology.com/geminidailyhoroscope.html";
-        super.loadHoroscope(link);
-        ImageButton share = (ImageButton) findViewById(R.id.shareContent);
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                shareLink();
-            }
-        });
+                            JSONObject jObj = new JSONObject(response);
+                            String horoscope = jObj.getString("description");
+                            textView.setText(horoscope);
+
+                        } catch (JSONException e) {
+                            Log.e("MYAPP", "unexpected JSON exception", e);
+                            // Do something to recover ... or kill the app.
+                        }
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        );
+        queue.add(postRequest);
+
     }
-    protected int getRId() {
-        return R.id.webViewGemini;
-    }
+
 }
+
